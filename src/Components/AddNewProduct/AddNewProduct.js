ee4df022ffcf28d6e4a8bc36f087ad1dd80535e7
alish <alifshania222@gmail.com>
2024-03-05 10:11:38 -0800

    style : add styles for add product component inputes

--- src/Components/AddNewProduct/AddNewProduct.js
+++ src/Components/AddNewProduct/AddNewProduct.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import "./AddNewProduct.css"
+import { PiCurrencyDollar } from "react-icons/pi";
+import { CiImageOn } from "react-icons/ci";
+import { SlWallet } from "react-icons/sl";
+import { IoColorPaletteOutline } from "react-icons/io5";
+import { MdOutlineDiscount } from "react-icons/md";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
+
+
 
 export default function AddNewProduct() {
   return (
@@ -8,28 +17,36 @@ export default function AddNewProduct() {
           <form className='add-product-form'>
                <div className='add-product-form-wrap'>
                     <div className='add-product-form-group'>
+                         <MdOutlineDriveFileRenameOutline/>
                          <input type='text' placeholder='اسم محصول را بنویسید' className='add-product-input'/>
                     </div>
                     <div className='add-product-form-group'>
+                         <PiCurrencyDollar/>
                          <input type='text' placeholder='قیمت محصول را بنویسید' className='add-product-input'/>
                     </div>
                     <div className='add-product-form-group'>
+                         <MdOutlineDiscount/>
                          <input type='text' placeholder='موجودی محصول را بنویسید' className='add-product-input'/>
                     </div>
                     <div className='add-product-form-group'>
+                         <CiImageOn/>
                          <input type='text' placeholder='  آدرس عکس محصول را بنویسید' className='add-product-input'/>
                     </div>
                     <div className='add-product-form-group'>
+                         <SlWallet/>
                          <input type='text' placeholder='میزان محصول را بنویسید' className='add-product-input'/>
                     </div>
                     <div className='add-product-form-group'>
+                         <SlWallet/>
                          <input type='text' placeholder='میزان فروش محصول را بنویسید' className='add-product-input'/>
                     </div>
                     <div className='add-product-form-group'>
+                         <IoColorPaletteOutline/>
                          <input type='text' placeholder='اتعداد رنگ بندی محصول را بنویسید' className='add-product-input'/>
                     </div>
                     
                </div>
+               <button className='add-product-btn' >ثبت محصول </button>
           </form>
     </div>
   )
