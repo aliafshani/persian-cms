63daf4d27499fa52933c0c4eb410b8df01eed03d
alish <alifshania222@gmail.com>
2024-03-05 23:34:18 -0800

    style : style product table

--- src/Components/ProductTable/ProductTable.js
+++ src/Components/ProductTable/ProductTable.js
@@ -5,12 +5,12 @@ export default function ProductTable() {
   return (
     <table className='product-table'>
       <tr className='product-table-heading-tr'>
-          <td>photo</td>
-          <td>name</td>          
-          <td>price</td>
-          <td>mojodi</td>
+          <th>photo</th>
+          <th>name</th>          
+          <th>price</th>
+          <th>mojodi</th>
       </tr>
-      <tr>
+      <tr className='product-table-tr'>
           <td>
                <img className='product-table-img' src='./Images/iphone15.webp' alt='image product'/>
           </td>
